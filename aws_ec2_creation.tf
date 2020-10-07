4fd25e29313346c00da73367c517e8d6bb7accc4
naren970 <narendra.palavelli@gmail.com>
2020-10-07 23:19:00 +0530

    Update aws_ec2_creation.tf

--- aws_ec2_creation.tf
+++ aws_ec2_creation.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "AKIAIWX67MZSJJUOJTAQ"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region     = "us-east-1"
 }
 
